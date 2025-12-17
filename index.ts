832c2b2b5328e0ae82f0ca8f7a02a3b9f30bf422
devZenta <hugo.ghesquier59@gmail.com>
2025-12-17 12:03:27 +0100

    feat: implement Elysia server with Swagger documentation and initial routes

--- index.ts
+++ index.ts
@@ -1,1 +1,34 @@
-console.log("Hello via Bun!");
+import { Elysia } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
+
+export const app = new Elysia()
+    .use(
+        swagger({
+            documentation: {
+                info: {
+                    title: 'MyHouse OS',
+                    version: '1.0.0'
+                }
+            },
+            path: '/swagger'
+        })
+    )
+    .get('/temp', () => 'OK')
+    .post('/temp', () => 'OK')
+
+    .get('/toggle/light', () => 'OK')
+    .post('/toggle/light', () => 'OK')
+
+    .get('/toggle/door', () => 'OK')
+    .post('/toggle/door', () => 'OK')
+
+    .get('/toggle/heat', () => 'OK')
+    .post('/toggle/heat', () => 'OK')
+
+    .get('/auth', () => 'OK')
+    .post('/auth', () => 'OK')
+
+    .listen(3000)
+
+console.log('🦊 serveur → http://localhost:3000')
+console.log('📖 swagger UI → http://localhost:3000/swagger')
