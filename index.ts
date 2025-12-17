ea0c0357facad544eafaaa5a4af9fb3da7ce17c6
francoisdotdev <lef02fran@gmail.com>
2025-12-17 15:07:09 +0100

    feat: integrate routes into the Elysia app and remove temporary route groups

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
+import { routes } from "./src/routes";
 
 export const app = new Elysia()
 	.use(
@@ -13,26 +14,7 @@ export const app = new Elysia()
 			path: "/swagger",
 		}),
 	)
-	.group("/temp", { detail: { tags: ["Temperature"] } }, (app) =>
-		app.get("/", () => "OK").post("/", () => "OK"),
-	)
-
-	.group("/toggle", { detail: { tags: ["Toggle"] } }, (app) =>
-		app
-			.get("/light", () => "OK")
-			.post("/light", () => "OK")
-
-			.get("/door", () => "OK")
-			.post("/door", () => "OK")
-
-			.get("/heat", () => "OK")
-			.post("/heat", () => "OK"),
-	)
-
-	.group("/auth", { detail: { tags: ["Authentication"] } }, (app) =>
-		app.get("/", () => "OK").post("/", () => "OK"),
-	)
-
+	.use(routes)
 	.listen(3000);
 
 console.log("🦊 serveur → http://localhost:3000");
