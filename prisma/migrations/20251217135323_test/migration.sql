9b8a9dd9fa757ce62186360756f7aba926597efd
francoisdotdev <lef02fran@gmail.com>
2025-12-17 15:06:39 +0100

    feat: add initial Prisma configuration, schema, and migration files for PostgreSQL

---
+++ prisma/migrations/20251217135323_test/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" SERIAL NOT NULL,
+    "ClientID" TEXT NOT NULL,
+    "ClientToken" TEXT NOT NULL,
+
+    CONSTRAINT "Client_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Client_ClientID_key" ON "Client"("ClientID");
