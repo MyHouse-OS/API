b9bb41428f64c16ee4f0b30785106d0a76a3551d
devZenta <hugo.ghesquier59@gmail.com>
2025-12-19 12:07:37 +0100

    refactor: remove unnecessary comments and clean up WebSocket connection logic

--- src/routes/ws/index.ts
+++ src/routes/ws/index.ts
@@ -7,7 +7,6 @@ export const wsRoutes = new Elysia().ws("/ws", {
 		console.log("WebSocket connected");
 		ws.subscribe("home-updates");
 
-		// Send initial state
 		const state = await HomeStateService.get();
 		ws.send({
 			type: "INIT",
@@ -16,17 +15,9 @@ export const wsRoutes = new Elysia().ws("/ws", {
 
 		eventBus.emit(EVENTS.NEW_CONNECTION);
 	},
-	message: () => {
-		// We don't expect messages from client for now, strictly push
-	},
+	message: () => {},
 	close: (ws) => {
 		console.log("WebSocket disconnected");
 		ws.unsubscribe("home-updates");
 	},
 });
-
-// Listener global : Quand le Service dit "Ça a bougé !", on broadcast via le serveur Bun
-// Note: On a besoin de l'instance 'app' pour appeler server.publish.
-// Comme on est dans un module, on ne l'a pas directement.
-// Astuce : On va utiliser une fonction d'initialisation ou s'appuyer sur le fait que
-// Elysia gère ça. Mais le plus simple est de le faire dans index.ts ou ici si on peut récupérer l'instance.
